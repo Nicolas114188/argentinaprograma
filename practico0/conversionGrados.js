36aef760cee5539cf761d4dd34e292928638e4da
unknown <nicolasalberto.herrera@gmail.com>
2023-05-20 12:35:10 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,10 @@
+//a: ¿Cuántos grados C son 50 F?
+let fahrenheit=50;
+let celsius;
+celsius=(fahrenheit-32)*(5/9);
+console.log(celsius);
+
+//b: ¿Cuántos grados C son 75 F?
+fahrenheit=75;
+celsius=(fahrenheit-32)*(5/9);
+console.log(celsius);
