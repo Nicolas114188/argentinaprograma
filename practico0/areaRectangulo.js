1e61096f57ee0f5bfcba2e4f56ae6a65939b58f9
unknown <nicolasalberto.herrera@gmail.com>
2023-05-23 17:26:39 -0300

    Agrego el ejercicio areaRectangulo

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,11 @@
+//a: rectangulo de base 5 cm y altura 3cm
+let base=5;
+let altura=3;
+let area=base*altura;
+console.log(area);
+
+//b: rectangulo de base 12.5 cm y altura 5.3cm
+base=12.5;
+altura=5.3;
+area=base*altura;
+console.log(area);
