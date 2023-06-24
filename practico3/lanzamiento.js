9573881846f790032977d6ac96dc4ccbca0ddbf7
unknown <nicolasalberto.herrera@gmail.com>
2023-06-24 13:20:37 -0300

    Ejercicio 1: Lanzamiento

---
+++ practico3/lanzamiento.js
@@ -0,0 +1,14 @@
+//Cuenta Regresiva
+let i=10;
+while(i>0){
+    if(i===10){
+        console.log("Cuenta regresiva 10");
+    }else{
+        console.log(i);
+    }
+    i--;
+}
+if(i===0){
+    console.log("Lanzamiento!");
+}
+
